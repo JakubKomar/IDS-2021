f7f4f8a575af4427b500e78708c4a860ebfe81a8
komca12 <57370991+komca12@users.noreply.github.com>
2021-03-20 01:35:17 +0100

    pár selectů

--- part2/test.sql
+++ part2/test.sql
@@ -99,9 +99,12 @@ CREATE TABLE workingOn(
 );
 
 
+--------------example data----------
+
 insert into person(birthNum,firstName,lastName,email,phoneNum,city,psc,street,streetNum) values (8003231379,'Carl','Johnson','cj@email.cz','+4201234-56789','San Andreas',12345,'grove street','127/I');
 insert into person(birthNum,firstName,email,phoneNum,city,psc,street,streetNum) values (6611241670,'honza','test2@email.cz','+420987654321','praha',54321,'nemám rád ponďělí','123');
-insert into person(birthNum,MIDNAME,firstName,lastName,email,phoneNum,city,psc,street,streetNum) values (7908031846,'Franta','Pepa','Jenička','franta@gmail.com','+4201454-56789','Brno',66345,'hradecká','62');
+insert into person(birthNum,firstName,email,phoneNum,city,psc,street,streetNum) values (6812247849,'Ráčna','test3@email.cz','+4204849421','ČB',45645,'sokolská','123');
+insert into person(birthNum,firstName,MIDNAME,lastName,email,phoneNum,city,psc,street,streetNum) values (7908031846,'Franta','Pepa','Jenička','franta@gmail.com','+4201454-56789','Brno',66345,'hradecká','62');
 SELECT * from person;
 
 INSERT INTO DEPARTMENT(nameD,descriptionD,room) VALUES ('main department','Toto je hlavní oddělení.',201);
@@ -112,10 +115,12 @@ insert into worker(workingIn,leads,birthNum,specialization,title,wage) VALUES (1
 SELECT * from worker;
 
 insert into myClient(birthNum) VALUES (6611241670);
+insert into myClient(birthNum) VALUES (6812247849);
 SELECT * from myClient;
 
 insert into myOrder(clientBN,workerBN,finalPrice,deadLine) values (6611241670,8003231379,20000,TO_DATE('2020/3/10','yyyy/mm/dd'));
 insert into myOrder(clientBN,workerBN,finalPrice,deadLine) values (6611241670,8003231379,60000,TO_DATE('2020/6/18','yyyy/mm/dd'));
+insert into myOrder(clientBN,workerBN,finalPrice,deadLine) values (6812247849,7908031846,70000,TO_DATE('2020/7/18','yyyy/mm/dd'));
 SELECT * from myOrder;
 
 insert into requirement(id,price,typeR,validity) values (1,5000,'Bilboard',TO_DATE('2020/3/10','yyyy/mm/dd'));
@@ -126,12 +131,29 @@ insert into requirement(id,price,typeR,validity) values (2,5000,'Bilboard',TO_DA
 insert into requirement(id,price,typeR,validity) values (2,6000,'propaganda',TO_DATE('2020/4/10','yyyy/mm/dd'));
 insert into requirement(id,price,typeR,validity,state,tempWorkers) values (2,7000,'Internet ad',TO_DATE('2020/2/10','yyyy/mm/dd'),'work in progress',3);
 insert into requirement(id,price,typeR,validity,state,tempWorkers) values (2,10000,'Magazine ad',TO_DATE('2020/1/10','yyyy/mm/dd'),'finished',5);
+
+insert into requirement(id,price,typeR,validity) values (3,5000,'plakát',TO_DATE('2020/3/10','yyyy/mm/dd'));
+insert into requirement(id,price,typeR,validity) values (3,6000,'něco',TO_DATE('2020/4/10','yyyy/mm/dd'));
+insert into requirement(id,price,typeR,validity,state,tempWorkers) values (3,7000,'nevím',TO_DATE('2020/2/10','yyyy/mm/dd'),'work in progress',3);
+insert into requirement(id,price,typeR,validity,state,tempWorkers) values (3,10000,'tohle',TO_DATE('2020/1/10','yyyy/mm/dd'),'finished',5);
+
 SELECT * from requirement;
 
 insert into invoice(makedBy,clientBN) values (8003231379,6611241670);
+insert into invoice(makedBy,clientBN) values (8003231379,6812247849);
+insert into invoice(makedBy,clientBN) values (8003231379,6812247849);
 SELECT * from invoice;
+
 UPDATE requirement SET invoice=1 WHERE (id,discriminator) in ((1,1),(1,3));
+UPDATE requirement SET invoice=2 WHERE (id,discriminator) in ((3,8),(3,9));
+UPDATE requirement SET invoice=3 WHERE (id,discriminator) in ((3,10),(3,11));
 SELECT * from requirement;
 
 insert into workingOn(requirementID,requirementDiscriminator,departmentKEY) VALUES (1,1,1);
 SELECT * from workingOn;
+
+--------------select----------------
+
+SELECT DISTINCT REQ.* FROM MYORDER O,REQUIREMENT REQ WHERE REQ.id=O.id AND O.id=1 ;  --zobraz všechny požadavky z objednávky č.1
+SELECT DISTINCT I.* FROM invoice I,MYCLIENT CLI WHERE CLI.birthNum=I.clientBN AND CLI.birthNum=6812247849;  --faktury uživatele s rodným číslem 6812247849
+SELECT DISTINCT REQ.* FROM requirement REQ,myOrder O,myClient CLI WHERE CLI.birthNum=O.clientBN AND CLI.birthNum=6611241670 AND O.id=REQ.id;    --zobrazí všechny požadavky klienta s rodným číslem 6611241670
